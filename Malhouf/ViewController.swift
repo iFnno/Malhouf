89d74b50f4ffb223bb2052ea6401f9c0c84ee9f4
iFnno <32503927+iFnno@users.noreply.github.com>
2018-08-02 10:27:13 +0300

    Initial Commit

---
+++ Malhouf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Malhouf
+//
+//  Created by Afnan S on 8/2/18.
+//  Copyright © 2018 hajjhackthon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
